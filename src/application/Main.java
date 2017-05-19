53acb01fd44a9a7e0603f63f17de6d910e496358
WISS <WISS@DELL-HUGO.hftm.ch>
2017-05-19 12:18:54 +0200

    test

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,11 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
+/**
+ * 
+ * @author WISS
+ *
+ */
 
 public class Main extends Application {
 	@Override
